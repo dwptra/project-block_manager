5e9db5576b617cd57764e1f60d50b878984b0f7e
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-04-18 10:28:16 +0700

    Menambah file env.example

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'project_id',
+        'page_name',
+        'note',
+        'status'
+    ];
+
+
+    public function projects()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
