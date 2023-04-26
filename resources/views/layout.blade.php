54039aec36c0e6cebe127489306da3820a961cfc
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-26 08:37:11 +0700

    Perbaikan pada footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -136,7 +136,7 @@
         ***********************************-->
         <div class="footer">
             <div class="copyright">
-                <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
+                <p>Copyright &copy; Designed & Developed by <b>Block Manager</b> 2023</p>
             </div>
         </div>
         <!--**********************************
