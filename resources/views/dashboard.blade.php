f5ad3807f0c82e4572457a6af2ef883e2df61a78
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-04-27 14:29:58 +0700

    ubah tampilan dashboard dan tambah menu baru dalam modul Block

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,56 +41,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Project List</h4>
-                        <div class="d-flex justify-content-end">
-                            <a class="btn btn-primary" href="{{ route('project.create') }}">Create New Project</a>
-                        </div>
-                        <div class="table-responsive">
-                            <table class="table table-striped table-bordered zero-configuration">
-                                <thead>
-                                    <?php
-                                        $i = 1;
-                                    ?>
-                                    <tr>
-                                        <th>NO</th>
-                                        <th>ID</th>
-                                        <th>Project Name</th>
-                                        <th>Project Manager</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($projectDB as $project)
-                                    <tr>
-                                        <td>{{ $i++ }}</td>
-                                        <td>{{ $project->id }}</td>
-                                        <td>{{ $project->project_name }}</td>
-                                        <td>{{ $project->project_manager }}</td>
-                                        <td>
-                                            <div class="d-flex">
-                                                <a class="btn btn-warning text-white mr-1" href="/page/{{ $project->id }}">See Pages</a> 
-                                                <a class="btn btn-primary mr-1" href="{{ route('project.edit', $project['id']) }}">Edit</a>
-                                                <form action="{{ route('project.delete', $project['id']) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus?')" type="submit">Delete</button>
-                                                </form>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>NO</th>
-                                        <th>ID</th>
-                                        <th>Project Name</th>
-                                        <th>Project Manager</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
+                        <h4 class="text-muted">Hi, {{Auth()->user()->name}}!</h4>
                     </div>
                 </div>
             </div>
