9d04faa4c3e010696a85291aa670942198b3a33e
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-04-27 14:21:27 +0700

    Tambah timestamps

--- database/migrations/2023_04_18_030200_create_block_categories_table.php
+++ database/migrations/2023_04_18_030200_create_block_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('block_categories', function (Blueprint $table) {
             $table->id();
             $table->char('category_name');
+            $table->timestamps();
         });
     }
 
