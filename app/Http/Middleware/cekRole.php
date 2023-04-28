e82aceccaaa3757586feb75957d6e5dd7515faca
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-04-28 14:55:43 +0700

    tambah middleware agar project manager tidak bisa membuat, mengedit dan menghapus user

---
+++ app/Http/Middleware/cekRole.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CekRole
+{
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (in_array(Auth::user()->role, $roles)){
+            return $next($request);
+        }
+        return redirect('/error');
+    }
+}
