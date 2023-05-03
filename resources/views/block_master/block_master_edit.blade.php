c22f1d70e8acf2d002088cb7632ad92824183978
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-03 15:57:23 +0700

    perbaiki posisi error di block

--- resources/views/block_master/block_master_edit.blade.php
+++ resources/views/block_master/block_master_edit.blade.php
@@ -1,14 +1,6 @@
 @extends('layout')
 @section('content')
-@if ($errors->any())
-    <div class="alert alert-danger alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
-        </button> <strong>Error:</strong>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </div>
-@endif
+
 <div class="container-fluid mt-3">
     <div class="container-fluid">
         <div class="row">
@@ -16,6 +8,15 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Create Block</h4>
+                        @if ($errors->any())
+                            <div class="alert alert-danger alert-dismissible fade show">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
+                                </button> <strong>Error:</strong>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        @endif
                         <div class="d-flex justify-content-end">
                             <a class="btn btn-danger ml-1" href="{{ route('block.master') }}">Back</a>
                         </div>
