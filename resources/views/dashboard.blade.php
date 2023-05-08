d7c97c987511ddf5e655a554e23c274ea979d7d8
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-08 11:54:54 +0700

    tambah total admin, pm, user dan project

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,47 +1,87 @@
 @extends('layout')
 @section('content')
-
-    @if (Session::get('isGuest'))
-    <div class="alert alert-primary alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                aria-hidden="true">&times;</span>
-        </button> <strong>Fail!</strong> {{ Session::get('isGuest')}}
-    </div>
-    @endif
-    @if (Session::get('createProject'))
-    <div class="alert alert-success alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                aria-hidden="true">&times;</span>
-        </button> <strong>Success!</strong> {{ Session::get('createProject')}}
-    </div>
-    @endif
-    @if (Session::get('updateProject'))
-    <div class="alert alert-success alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                aria-hidden="true">&times;</span>
-        </button> <strong>Success!</strong> {{ Session::get('updateProject')}}
-    </div>
-    @endif
-    @if (Session::get('deleteProject'))
-    <div class="alert alert-success alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                aria-hidden="true">&times;</span>
-        </button> <strong>Success!</strong> {{ Session::get('deleteProject')}}
-    </div>
-    @endif
-    @if (Session::get('successLogin'))
-    <div class="alert alert-success alert-dismissible fade show">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                aria-hidden="true">&times;</span>
-        </button> <strong>Success!</strong> {{ Session::get('successLogin')}}
-    </div>
-    @endif
-    <div id="app">
-        <div class="main-wrapper main-wrapper-1">
-            <section class="section">
-                <div class="section-header">
-                    <h1>Dashboard</h1>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        <section class="section">
+            <div class="section-header">
+                <h1>Dashboard</h1>
+            </div>    
+            @if (Session::get('isGuest'))
+            <div class="alert alert-danger alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span>
+                </button> <strong>Fail!</strong> {{ Session::get('isGuest')}}
+            </div>
+            @endif
+            @if (Session::get('successLogin'))
+            <div class="alert alert-success alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span>
+                </button> <strong>Success!</strong> {{ Session::get('successLogin')}}
+            </div>
+            @endif
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                      <i class="far fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Total Admin</h4>
+                      </div>
+                      <div class="card-body">
+                        {{$totalAdmin}}
+                      </div>
+                    </div>
+                  </div>
                 </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                      <i class="far fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Total Project Manager</h4>
+                      </div>
+                      <div class="card-body">
+                        {{$totalPM}}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                      <div class="card-icon bg-success">
+                        <i class="far fa-user"></i>
+                      </div>
+                      <div class="card-wrap">
+                        <div class="card-header">
+                          <h4>Total User</h4>
+                        </div>
+                        <div class="card-body">
+                          {{$totalUser}}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                      <i class="far fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Reports</h4>
+                      </div>
+                      <div class="card-body">
+                        {{$totalProject}}
+                      </div>
+                    </div>
+                  </div>
+                </div>                 
+              </div>
             </section>
         </div>
     </div>
