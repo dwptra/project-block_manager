f7b50b38d2069c428b63f51ec1f1b2d914678375
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-08 10:17:49 +0700

    perbaiki posisi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,32 @@
 @extends('layout')
 @section('content')
-<div class="container-fluid mt-3">
+
     @if (Session::get('isGuest'))
     <div class="alert alert-primary alert-dismissible fade show">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
                 aria-hidden="true">&times;</span>
         </button> <strong>Fail!</strong> {{ Session::get('isGuest')}}
+    </div>
+    @endif
+    @if (Session::get('createProject'))
+    <div class="alert alert-success alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                aria-hidden="true">&times;</span>
+        </button> <strong>Success!</strong> {{ Session::get('createProject')}}
+    </div>
+    @endif
+    @if (Session::get('updateProject'))
+    <div class="alert alert-success alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                aria-hidden="true">&times;</span>
+        </button> <strong>Success!</strong> {{ Session::get('updateProject')}}
+    </div>
+    @endif
+    @if (Session::get('deleteProject'))
+    <div class="alert alert-success alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                aria-hidden="true">&times;</span>
+        </button> <strong>Success!</strong> {{ Session::get('deleteProject')}}
     </div>
     @endif
     @if (Session::get('successLogin'))
@@ -15,11 +36,13 @@
         </button> <strong>Success!</strong> {{ Session::get('successLogin')}}
     </div>
     @endif
-    <section class="section">
-        <div class="section-header">
-          <h1>Dashboard</h1>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <section class="section">
+                <div class="section-header">
+                    <h1>Dashboard</h1>
+                </div>
+            </section>
         </div>
-      </section>
-    <!-- #/ container -->
-</div>
+    </div>
 @endsection
