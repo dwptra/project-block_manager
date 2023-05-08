d4f92048ba490d086947b8ae6762664e7fbb17f8
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-05-08 13:43:14 +0700

    benerin tampilan category

--- resources/views/blocks/block_categories.blade.php
+++ resources/views/blocks/block_categories.blade.php
@@ -5,90 +5,100 @@
         display: inline-block;
         min-width: 125px;
     }
+
 </style>
-<div class="container-fluid mt-3">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                @if (Session::get('createCategory'))
-                <div class="alert alert-success alert-dismissible fade show">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                            aria-hidden="true">&times;</span>
-                    </button> <strong>Success!</strong> {{ Session::get('createCategory')}}
-                </div>
-                @endif
-                @if (Session::get('updateCategory'))
-                <div class="alert alert-success alert-dismissible fade show">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                            aria-hidden="true">&times;</span>
-                    </button> <strong>Success!</strong> {{ Session::get('updateCategory')}}
-                </div>
-                @endif
-                @if (Session::get('deleteCategory'))
-                <div class="alert alert-success alert-dismissible fade show">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                            aria-hidden="true">&times;</span>
-                    </button> <strong>Success!</strong> {{ Session::get('deleteCategory')}}
-                </div>
-                @endif
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Categories List</h4>
-                        <div class="d-flex justify-content-end">
-                            <button class="btn btn-primary" data-toggle="modal" data-target="#createCategories">Create Category</button>
-                        </div>
-                        <div class="table-responsive mt-3">
-                            <table class="table table-striped table-bordered zero-configuration">
-                                <thead>
-                                    <tr>
-                                        <th style="max-width: 60px;">#</th>
-                                        <th>Category Name</th>
-                                        <th>Created_at</th>
-                                        <th>Updated_at</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($categoriesDB as $category)
-                                    <tr>
-                                        <td class="text-center">{{ $category->id }}</td>
-                                        <td>{{ $category->category_name }}</td>
-                                        <td>{{ $category->created_at->format('Y-m-d') }}</td>
-                                        <td>{{ $category->updated_at->format('Y-m-d') }}</td>
-                                        <td>
-                                            <div class="d-flex">
-                                                <button class="btn btn-primary mr-1" data-toggle="modal" data-target="#editCategories{{ $category->id }}">Edit</button>
-                                                <form action="{{ route('category.delete', $category->id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="btn btn-danger"
-                                                        onclick="return confirm('Yakin ingin menghapus?')"
-                                                        type="submit">Delete</button>
-                                                </form>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @empty
-                                    <tr>
-                                        <td colspan="5" class="text-center">No pages found.</td>
-                                    </tr>
-                                    @endforelse
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th style="max-width: 60px;">#</th>
-                                        <th>Category Name</th>
-                                        <th>Created_at</th>
-                                        <th>Updated_at</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
+<div class="app">
+    <div class="main-wrapper main-wrapper-1">
+        <section class="section">
+            <div class="section-header">
+                <h1>Categories List</h1>
+            </div>
+            @if (Session::get('createCategory'))
+            <div class="alert alert-success alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span>
+                </button> <strong>Success!</strong> {{ Session::get('createCategory')}}
+            </div>
+            @endif
+            @if (Session::get('updateCategory'))
+            <div class="alert alert-success alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span>
+                </button> <strong>Success!</strong> {{ Session::get('updateCategory')}}
+            </div>
+            @endif
+            @if (Session::get('deleteCategory'))
+            <div class="alert alert-success alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span>
+                </button> <strong>Success!</strong> {{ Session::get('deleteCategory')}}
+            </div>
+            @endif
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-end">
+                                    <button class="btn btn-primary" data-toggle="modal" data-target="#createCategories">Create
+                                        Category</button>
+                                </div>
+                                <div class="table-responsive mt-3">
+                                    <table class="table table-striped table-bordered zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th style="max-width: 60px;">#</th>
+                                                <th>Category Name</th>
+                                                <th>Created_at</th>
+                                                <th>Updated_at</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($categoriesDB as $category)
+                                            <tr>
+                                                <td class="text-center">{{ $category->id }}</td>
+                                                <td>{{ $category->category_name }}</td>
+                                                <td>{{ $category->created_at->format('Y-m-d') }}</td>
+                                                <td>{{ $category->updated_at->format('Y-m-d') }}</td>
+                                                <td>
+                                                    <div class="d-flex">
+                                                        <button class="btn btn-primary mr-1" data-toggle="modal"
+                                                            data-target="#editCategories{{ $category->id }}">Edit</button>
+                                                        <form action="{{ route('category.delete', $category->id) }}"
+                                                            method="post">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button class="btn btn-danger"
+                                                                onclick="return confirm('Yakin ingin menghapus?')"
+                                                                type="submit">Delete</button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            @empty
+                                            <tr>
+                                                <td colspan="5" class="text-center">No pages found.</td>
+                                            </tr>
+                                            @endforelse
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th style="max-width: 60px;">#</th>
+                                                <th>Category Name</th>
+                                                <th>Created_at</th>
+                                                <th>Updated_at</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 </div>
 
@@ -115,7 +125,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Create</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">Close</button>
                 </div>
             </form>
         </div>
@@ -124,8 +134,8 @@
 
 {{-- edit --}}
 @foreach ($categoriesDB as $category)
-<div class="modal fade" id="editCategories{{ $category->id }}" tabindex="-1" role="dialog" aria-labelledby="editCategoriesLabel"
-    aria-hidden="true">
+<div class="modal fade" id="editCategories{{ $category->id }}" tabindex="-1" role="dialog"
+    aria-labelledby="editCategoriesLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -140,12 +150,13 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Category Name:</label>
-                        <input name="category_name" type="text" class="form-control" id="recipient-name" value="{{ $category->category_name }}">
+                        <input name="category_name" type="text" class="form-control" id="recipient-name"
+                            value="{{ $category->category_name }}">
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Save</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <a href="{{ route('block.categories') }}" type="button" class="btn btn-secondary">Close</a>
                 </div>
             </form>
         </div>
