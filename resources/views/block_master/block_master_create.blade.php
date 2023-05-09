eafdd5cfa05f27decd4c70302963e195cbd54331
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-09 10:22:00 +0700

    tambah old value di select

--- resources/views/block_master/block_master_create.blade.php
+++ resources/views/block_master/block_master_create.blade.php
@@ -59,7 +59,7 @@
                                             name="category_id">
                                             <option value="" selected disabled>Select Category Block</option>
                                             @foreach($blockCategoryCreate as $category)
-                                            <option value="{{$category->id}}">{{$category->category_name}}</option>
+                                            <option value="{{$category->id}}" {{ old('category_id') == $category->id ? 'selected' : '' }}>{{$category->category_name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
