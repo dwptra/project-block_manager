418c3fdc7a57dea4271605f27ec4d3106dc565ef
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-19 13:27:17 +0700

    Tambah informasi page, project manager, dan block pada masing masing create dan edit

--- resources/views/blocks/block_edit.blade.php
+++ resources/views/blocks/block_edit.blade.php
@@ -4,47 +4,47 @@
     <div class="main-wrapper main-wrapper-1">
         <section class="section">
             <div class="section-header">
-                <h1>Block Edit</h1>     
+                <h1>Block Edit</h1>
             </div>
-        <div class="row">
-            <div class="col-12">
-                @if ($errors->any())
-                <div class="alert alert-danger alert-dismissible fade show">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                            aria-hidden="true">&times;</span>
-                    </button> <strong>Error:</strong>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </div>
-                @endif
-                <div class="card">
-                    <div class="card-body">
-                        <form action="{{ route('block.update', $blockEdit['id'])}}" method="post">
-                        <div class="d-flex justify-content-end">
-                            <button type="submit" class="btn btn-primary mr-1">Save Block</button>
-                            <a class="btn btn-danger ml-1" href="{{ route('block', $page->id) }}">Back</a>
-                        </div>
-                        <hr>
+            <div class="row">
+                <div class="col-12">
+                    @if ($errors->any())
+                    <div class="alert alert-danger alert-dismissible fade show">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                aria-hidden="true">&times;</span>
+                        </button> <strong>Error:</strong>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                    @endif
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('block.update', $blockEdit['id'])}}" method="post">
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary mr-1">Save Block</button>
+                                <a class="btn btn-danger ml-1" href="{{ route('block', $page->id) }}">Back</a>
+                            </div>
+                            <hr>
                             @csrf
                             @method('PATCH')
                             <div class="form-group row mb-1">
-                                <label class="col-sm-2 col-form-label"><b>Project Name</b><span class="text-danger">*</span></label>
+                                <label class="col-sm-2 col-form-label"><b>Project Name</b></label>
                                 <div class="col-sm-10">
-                                    <span style="font-size: 14px;">: {{ $page->projects->project_name }}</span>
+                                    <span>: {{ $page->projects->project_name }}</span>
                                 </div>
                             </div>
                             <div class="form-group row mb-2">
-                                <label class="col-sm-2 col-form-label"><b>Project Manager</b><span class="text-danger">*</span></label>
+                                <label class="col-sm-2 col-form-label"><b>Page Name</b></label>
                                 <div class="col-sm-10">
-                                    <span style="font-size: 14px;">: {{ $page->projects->projectManager->name }}</span>
+                                    <span>: {{ $page->page_name }}</span>
                                 </div>
                             </div>
                             <div class="form-group row mb-2">
-                                <label class="col-sm-2"><b>Section Name</b><span class="text-danger">*</span></label>
+                                <label class="col-sm-2"><b>Section Name </b><span class="text-danger">*</span></label>
                                 <div class="col-sm-10">
                                     <div class="input-group">
-                                        <input type="text" name="section_name" class="form-control" placeholder="Enter Section Name"  value="{{ $blockEdit->section_name}}" required>
+                                        <input type="text" name="section_name" class="form-control" placeholder="Enter Section Name" value="{{ $blockEdit->section_name}}" required>
                                     </div>
                                 </div>
                             </div>
@@ -57,7 +57,7 @@
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-sm-2 col-form-label"><b>Sort</b><span class="text-danger">*</span></label>
+                                <label class="col-sm-2 col-form-label"><b>Sort </b><span class="text-danger">*</span></label>
                                 <div class="col-sm-10">
                                     <div class="input-group">
                                         <input type="text" name="sort" class="form-control" placeholder="Enter Section Sort" value="{{ $blockEdit->sort}}" required>
@@ -82,15 +82,28 @@
                                                     @foreach ($blocks as $block)
                                                     <div class="card mx-4 mb-4" style="width: 18rem;">
                                                         <div class="gallery gallery-fw" data-item-height="300">
-                                                            <div class="gallery-item" data-image="{{ asset('storage/images/main_image/' . basename($block->main_image)) }}" data-title="Image 1"></div>
+                                                            <div class="gallery-item"
+                                                                data-image="{{ asset('storage/images/main_image/' . basename($block->main_image)) }}"
+                                                                data-title="Image 1"></div>
                                                             {{-- hide --}}
-                                                            <div class="gallery-item gallery-hide" data-image="{{ asset('storage/images/mobile_image/' . basename($block->mobile_image)) }}" data-title="Image 2"></div>
-                                                            <div class="gallery-item gallery-hide" data-image="{{ asset('storage/images/sample_image_1/' . basename($block->sample_image_1)) }}" data-title="Image 3"></div>
-                                                            <div class="gallery-item gallery-hide" data-image="{{ asset('storage/images/sample_image_2/' . basename($block->sample_image_2)) }}" data-title="Image 4"></div>
+                                                            <div class="gallery-item gallery-hide"
+                                                                data-image="{{ asset('storage/images/mobile_image/' . basename($block->mobile_image)) }}"
+                                                                data-title="Image 2"></div>
+                                                            <div class="gallery-item gallery-hide"
+                                                                data-image="{{ asset('storage/images/sample_image_1/' . basename($block->sample_image_1)) }}"
+                                                                data-title="Image 3"></div>
+                                                            <div class="gallery-item gallery-hide"
+                                                                data-image="{{ asset('storage/images/sample_image_2/' . basename($block->sample_image_2)) }}"
+                                                                data-title="Image 4"></div>
                                                         </div>
                                                         <div class="card-body">
-                                                            <input type="radio" class="selectgroup-input"  name="block_id" id="option{{ $block->id }}" autocomplete="off" value="{{ $block->id }}" {{ $block->id == $blockEdit->block_id ? 'checked' : '' }} />
-                                                            <label class="selectgroup-button btn btn-light align-center w-100 mb-0" for="option{{ $block->id }}">{{ $block->block_name }}</label>
+                                                            <input type="radio" class="selectgroup-input"
+                                                                name="block_id" id="option{{ $block->id }}"
+                                                                autocomplete="off" value="{{ $block->id }}"
+                                                                {{ $block->id == $blockEdit->block_id ? 'checked' : '' }} />
+                                                            <label
+                                                                class="selectgroup-button btn btn-light align-center w-100 mb-0"
+                                                                for="option{{ $block->id }}">{{ $block->block_name }}</label>
                                                         </div>
                                                     </div>
                                                     @endforeach
@@ -101,13 +114,13 @@
                                     @endforeach
                                 </div>
                             </div>
-                        </form>
-                        <hr>
-                    </div>
+                    </form>
+                    <hr>
                 </div>
             </div>
-        </div>
     </div>
+</div>
+</div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -123,5 +136,6 @@
             accordionItem.find('.accordion-header').attr('aria-expanded', 'true');
         }
     });
+
 </script>
 @endsection
--- resources/views/pages/page_edit.blade.php
+++ resources/views/pages/page_edit.blade.php
@@ -4,71 +4,87 @@
     <div class="main-wrapper main-wrapper-1">
         <section class="section">
             <div class="section-header">
-                <h1>Page Edit</h1>     
+                <h1>Page Edit</h1>
             </div>
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="form-validation">
-                        <form class="form-valide" action="{{ route('page.update', $pageDB['id']) }}" method="post">
-                        <div class="d-flex justify-content-end">
-                            <button type="submit" class="btn btn-primary mr-1">Save Page</button>
-                            <a class="btn btn-danger" href="{{ route('page', $pageDB->project_id) }}">Back</a>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-validation">
+                            <form class="form-valide" action="{{ route('page.update', $pageDB['id']) }}" method="post">
+                                <div class="d-flex justify-content-end">
+                                    <button type="submit" class="btn btn-primary mr-1">Save Page</button>
+                                    <a class="btn btn-danger" href="{{ route('page', $pageDB->project_id) }}">Back</a>
+                                </div>
+                                <hr>
+                                <div class="form-group row mb-1">
+                                    <label class="col-lg-4 col-form-label">Project Name</label>
+                                    <div class="col-lg-6">
+                                        <span>: {{ $project->project_name }}</span>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-2">
+                                    <label class="col-lg-4 col-form-label">Project Manager</label>
+                                    <div class="col-lg-6">
+                                        <span>: {{ $project->projectManager->name }}</span>
+                                    </div>
+                                </div>
+                                @csrf
+                                @method('PATCH')
+                                @if ($errors->any())
+                                <div class="alert alert-danger alert-dismissible fade show">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                            aria-hidden="true">&times;</span>
+                                    </button> <strong>Error:</strong>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </div>
+                                @endif
+                                <div class="form-group row">
+                                    <label class="col-lg-4 col-form-label" for="val-page-name">Page Name <span
+                                            class="text-danger">*</span>
+                                    </label>
+                                    <div class="col-lg-6">
+                                        <input name="page_name" type="text" class="form-control" id="val-page-name"
+                                            name="val-page-name" placeholder="Enter page-name.." required
+                                            value="{{ $pageDB->page_name }}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-4 col-form-label" for="val-note">Note
+                                    </label>
+                                    <div class="col-lg-6">
+                                        <textarea name="note" class="form-control" id="val-note" name="val-note"
+                                            rows="5" placeholder="Not Required">{{ $pageDB->note }}</textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-4 col-form-label" for="val-skill">Status <span
+                                            class="text-danger">*</span>
+                                    </label>
+                                    <div class="col-lg-6">
+                                        <select class="form-control selectric" aria-label="Default select example"
+                                            name="status">
+                                            <option value="On Progress"
+                                                {{ $pageDB->status == 'On Progress' ? 'selected' : '' }}>On Progress
+                                            </option>
+                                            <option value="On Review"
+                                                {{ $pageDB->status == 'On Review' ? 'selected' : '' }}>On Review
+                                            </option>
+                                            <option value="Approved"
+                                                {{ $pageDB->status == 'Approved' ? 'selected' : '' }}>
+                                                Approved</option>
+                                            <option value="Declined"
+                                                {{ $pageDB->status == 'Declined' ? 'selected' : '' }}>
+                                                Declined</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <hr>
-                            @csrf
-                            @method('PATCH')
-                            @if ($errors->any())
-                            <div class="alert alert-danger alert-dismissible fade show">
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                                        aria-hidden="true">&times;</span>
-                                </button> <strong>Error:</strong>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </div>
-                            @endif
-                            <div class="form-group row">
-                                <label class="col-lg-4 col-form-label" for="val-page-name">Page Name <span
-                                        class="text-danger">*</span>
-                                </label>
-                                <div class="col-lg-6">
-                                    <input name="page_name" type="text" class="form-control" id="val-page-name"
-                                        name="val-page-name" placeholder="Enter page-name.." required
-                                        value="{{ $pageDB->page_name }}">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-4 col-form-label" for="val-note">Note
-                                </label>
-                                <div class="col-lg-6">
-                                    <textarea name="note" class="form-control" id="val-note" name="val-note" rows="5"
-                                        placeholder="Not Required">{{ $pageDB->note }}</textarea>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-lg-4 col-form-label" for="val-skill">Status <span
-                                        class="text-danger">*</span>
-                                </label>
-                                <div class="col-lg-6">
-                                    <select class="form-control selectric" aria-label="Default select example" name="status">
-                                        <option value="On Progress"
-                                            {{ $pageDB->status == 'On Progress' ? 'selected' : '' }}>On Progress
-                                        </option>
-                                        <option value="On Review"
-                                            {{ $pageDB->status == 'On Review' ? 'selected' : '' }}>On Review</option>
-                                        <option value="Approved" {{ $pageDB->status == 'Approved' ? 'selected' : '' }}>
-                                            Approved</option>
-                                        <option value="Declined" {{ $pageDB->status == 'Declined' ? 'selected' : '' }}>
-                                            Declined</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
-        </div>
         </section>
     </div>
 </div>
