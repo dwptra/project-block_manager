ef6b896db8c9eee9c440cbdb196d0c3d9901c4e8
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-05-22 10:21:04 +0700

    tambah modal delete user

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -122,7 +122,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createCategoriesLabel">Delete Project</h5>
+                    <h5 class="modal-title" id="createCategoriesLabel">Delete User</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span>
                     </button>
